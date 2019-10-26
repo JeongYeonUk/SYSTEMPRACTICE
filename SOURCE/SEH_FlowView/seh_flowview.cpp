d1444084a8f3bd15f956b4eef0602b3eca425d66
연욱 정 <vire3064@gmail.com>
2019-10-26 16:36:08 +0900

    191026_1636

---
+++ SOURCE/SEH_FlowView/seh_flowview.cpp
@@ -0,0 +1,26 @@
+#pragma warning (disable : 4996)
+#include <stdio.h>
+#include <tchar.h>
+#include <Windows.h>
+#include <process.h>
+
+int _tmain(int argc, TCHAR* argv[])
+{
+	_tprintf(_T("start point! \n"));
+
+	int* p = NULL;
+
+	__try
+	{
+		*p = 100;
+		_tprintf(_T("value: %d \n"), *p);
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+		_tprintf(_T("exception occurred! \n"));
+	}
+
+	_tprintf(_T("end point! \n"));
+
+	return 0;
+}
